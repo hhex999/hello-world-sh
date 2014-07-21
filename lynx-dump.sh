c8e967783b9901eb39dab3b3e3b5ab2df48e6d77
hhex999 <hhex999@gmail.com>
2014-07-21 17:51:34 +0400

    message

--- lynx-dump.sh
+++ lynx-dump.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 for i in *.htm *.html ; do
-  lynx -dump "$i" > "${i}.txt"
+  lynx -dump "$i" > "${i}.txt" || echo "operation failed..."
 done
+
+rm \*.html.txt
+echo "Anyway, we are DONE, so check out"
+ls -lh
 
