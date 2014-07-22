56d99b1e06e8b06d0a0bd253732645f61f1f9297
hhex999 <hhex999@gmail.com>
2014-07-23 01:31:57 +0400

    message

--- gajim-ubuntu.sh
+++ gajim-ubuntu.sh
@@ -5,16 +5,33 @@
 #*********************************************************************
 
 # Установлен ли у нас mercurial -- он нам понадобится для
-# скачивания исходников (не релизов, а dev ветки разработки) из
-# репозитория hg.gajim.org
+# скачивания исходного кода (не релиза, а dev ветки разработки) из
+# Mercurial репозитория hg.gajim.org
 sudo apt-get install mercurial
+
+# кстати, эти зависимости, что мы в прошлый раз устанавливали, я бы их
+# все удалил и установил заново только те, что нужны для сборки gajim
+#    Runtime Requirements
+#  * python2.5 or higher
+#  * pygtk2.22 or higher
+#  * python-nbxmpp
+#
+#    Compile-time Requirements
+#  * intltool (>= 0.40.1)
+#
+#sudo apt-get install intltool python-gtk2-dev python-xmpp python-pyxmpp
+
+sudo apt-get remove  intltool python-gtk2-dev python-xmpp python-pyxmpp
+sudo apt-get install intltool python-gtk2-dev
+sudo apt-get autoremove
 
 # Set certificate fingerprint: Add the folowing fingerprint for
 # server hg.gajim.org into the ~/.hgrc Mercurial configuration file.
+
 #[hostfingerprints]
 #hg.gajim.org = 38:8d:67:e8:ce:76:7f:75:3d:ed:9b:b2:4a:06:aa:71:4c:14:fe:da
 
-# исходя из комментария выше ^^^ взятого на где-то на ... http://hg.gajim.org
+# исходя из комментария выше ^^^ взятого где-то на ... https://hg.gajim.org
 # нам нужно занести "отпечаток сертификата" для сервера hg.gajim.org
 # в ~/.hgrc конфигурационный файл Mercurial, делаем :
 
@@ -27,8 +44,8 @@ echo "hg.gajim.org = 38:8d:67:e8:ce:76:7f:75:3d:ed:9b:b2:4a:06:aa:71:4c:14:fe:da
 # abort: certificate for hg.gajim.org has unexpected fingerprint
 # 77:3b:82:49:eb:e0:92:b5:fd:46:af:fd:50:50:b7:9b:cd:55:9d:8a
 
-# то тогда видимо сменился "отпечаток сертификата" сервера ((у меня
-# так и было, выдало ошибку)) и нам нужно заменить строчку в конфиге
+# то тогда видимо сменился "отпечаток сертификата" сервера (у меня
+# так и было, выдало ошибку) и нам нужно заменить строчку в конфиге
 # ~/.hgrc с этой :
 hg.gajim.org = 38:8d:67:e8:ce:76:7f:75:3d:ed:9b:b2:4a:06:aa:71:4c:14:fe:da
 # на эту :
@@ -40,7 +57,7 @@ echo "" > ~/.hgrc
 
 # остальные две команды занесут 2 строчки в пустой конфиг :
 echo "[hostfingerprints]" >> ~/.hgrc
-echo "hg.gajim.org = 38:8d:67:e8:ce:76:7f:75:3d:ed:9b:b2:4a:06:aa:71:4c:14:fe:da" >> ~/.hgrc
+echo "hg.gajim.org = 77:3b:82:49:eb:e0:92:b5:fd:46:af:fd:50:50:b7:9b:cd:55:9d:8a" >> ~/.hgrc
 
 # ну и пробуем получить исходники для python-nbxmpp :
 
@@ -66,21 +83,47 @@ cd ~/build
 # создание архива
 tar -caf python-nbxmpp.tar.xz python-nbxmpp
 
-# теперь можно собрать python-nbxmpp
+# теперь можно собрать и установить python-nbxmpp
 # переход в директорию
-cd python-nbxmpp
-# и собственно установка в систему
+cd ~/build/python-nbxmpp
+# читаем файл README внутри диретории python-nbxmpp (`q` для выхода из утилиты `less`)
+less README
+# исходя из README процедура установки будет следующей
+# установка в систему
 sudo python setup.py install
 
+# а процедура удаления из системы (нужно читать доки по python distutils)
+# думаю логично предположить что
+cd ~/build/python-nbxmpp
+sudo python setup.py uninstall
+
+
+
+
+############################################################
+# НЕ РОБОТАТЬ (пока не нужно)
+############################################################
 # клонируем дерево исходного кода для gajim?
 # переход в директорию сборки
 cd ~/build
-# клонируем gajim в альтернативную директорию gajim-mercurial на всякий случай
+# клонируем gajim в альтернативную директорию gajim-mercurial (на всякий
+# случай во избежание коллизий имен директорий)
 hg clone https://hg.gajim.org/gajim gajim-mercurial
 
 # итак, нам бы сделать архив-бэкап склонированного репозитория
 tar -caf gajim-mercurial.tar.xz gajim-mercurial
 
+cd ~/build/gajim-mercurial
+less ChangeLog
+# и видим такие строчки
+
+#Gajim 0.16 ()
+#
+#  * Improve File transfer support by adding Jingle file transfer
+#  * use external python-nbxmpp library
+############################################################
+# НЕ РОБОТАТЬ (пока не нужно)
+############################################################
 # ну и судя комментариям на том же hg.gajim.org
 # если у нас дерево исходного кода из репозитория Mercurial (hg.gajim.org)
 # а не архив, скачанный с http://gajim.org
@@ -97,73 +140,60 @@ tar -caf gajim-mercurial.tar.xz gajim-mercurial
 # конфигов для gajim
 # ....it creates a new profile to avoid interferences between any
 # ....existing profile. muzzol
+############################################################
+# НЕ РОБОТАТЬ (пока не нужно)
+############################################################
+
+
 
 #*********************************************************************
-#*********************************************************************
-#*********************************************************************
-
-
-#*********************************************************************
-# отсюда начинается сбока gajim из релизного архива
-# в нашем случае это ( gajim-0.16-rc2.tar.gz )
-# Я тут пока не дописал, чуть позже допишу
-# тут еще надо будет с зависимостями со всеми разобраться (что куда зачем)
+# отсюда начинается сбока gajim
+# gajim-0.16-rc2.tar.gz
 #*********************************************************************
 
-#gajim-0.16-rc2.tar.gz
+# если повторю тут пару пакетов что были уже установлены ранее --
+# ничего страшного, повторно они не установятся все равно.
+# Это вроде как полный спикок зависимостей, как необходимых для сборки,
+# так и дополнительные (для корректной работы некоторых функций gajim)
 
-# кстати, эти зависимости, что мы в прошлый раз устанавливали, я бы их
-# все удалил и установил заново только те, что нужны для сборки gajim
-#    Runtime Requirements
-#  * python2.5 or higher
-#  * pygtk2.22 or higher
-#  * python-nbxmpp
-#
-#    Compile-time Requirements
-#  * intltool (>= 0.40.1)
-#
-# так что поступим так : 
-# sudo apt-get install intltool python-gtk2-dev python-xmpp python-pyxmpp
+sudo apt-get install   python python2.7 python3 python-gtk2-dev intltool python-openssl python3-openssl python-pyasn1 python-pyasn1-modules python3-pyasn1 python-crypto python3-crypto libxss1 python-avahi dnsutils aspell-en aspell-ru libgtkspell0 libgtkspell3-3-0 python-gtkspell python-gtkspellcheck python3-gtkspellcheck python-gnome2 notification-daemon python-notify python-notify2 python3-notify2 dbus-x11 dbus libdbus-1-3 libdbus-glib-1-2 libdbusmenu-glib4
 
-sudo apt-get remove  intltool python-gtk2-dev python-xmpp python-pyxmpp
-sudo apt-get install intltool python-gtk2-dev
-
-#*********************************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-cd
-mkdir ~/build
-
+# поехали
+# переход в директорию
 cd ~/build
+
+# распаковываем (TODO: если есть уже директория с этим именем и бинарник
+# установлен в систему -- обычно есть файлы в /usr/local/bin, то не удалять
+# директорию, сначала выполнить процедуру удаления пакета из системы и
+# затем только удалять папку, иначе при распаковке -- команда ниже с
+# ключами `-xf` просто перезапишет файлы при распаковке)
 tar -xf gajim-0.16-rc2.tar.gz
+
+# заходим в распакованную директорию
 cd ~/build/gajim-0.16-rc2
+
+# запуск скрипта конфигурирования
 ./configure --prefix=/usr/local
+
+# запуск сборки пакета
 make
-sudo make install && sudo ldconfig
 
-cd ~/build/gajim-0.16-rc2
-sudo make uninstall && sudo ldconfig
+# установка в систему и пересчет сист. файлов и библиотек
+sudo make install
+sudo ldconfig
 
-ls -l /usr/local/bin/
-cd ~/build && rm -rf gajim-0.16-rc2
+# это процедура удаления из системы, нужно размаскировать при необходимости
+#cd ~/build/gajim-0.16-rc2
+#sudo make uninstall
+#sudo ldconfig
 
+# это для проверки, смотрим в директорию куда у нас было установлено,
+# и если там пусто то удаляем папку с исходным кодом
+#ls -l /usr/local/bin/
+#cd ~/build
+#rm -rf gajim-0.16-rc2
 
+############################################################
  #1. tar jxvf gajim-version.tar.bz2
  #2. cd gajim
  #3. ./configure
@@ -172,103 +202,23 @@ cd ~/build && rm -rf gajim-0.16-rc2
 
 #To specify where to install do:
     #./configure --prefix=custom_path
+############################################################
 
-python - interactive high-level object-oriented language (default version)
-python2.7 - Interactive high-level object-oriented language (version 2.7)
-python2.7-dev - Header files and a static library for Python (v2.7)
-python3 - interactive high-level object-oriented language (default python3 version)
-python3-all-dev - package depending on all supported Python 3 development packages
-python3.4 - Interactive high-level object-oriented language (version 3.4)
-python3.4-dbg - Debug Build of the Python Interpreter (version 3.4)
-python3.4-dev - Header files and a static library for Python (v3.4)
-python3.4-doc - Documentation for the high-level object-oriented language Python (v3.4)
-python3.4-examples - Examples for the Python language (v3.4)
-python3.4-minimal - Minimal subset of the Python language (version 3.4)
+# ? фиг знает, когда искал зависимости, то вот нашел, не уверен нужны
+# ли эти пакеты
+#libcrypto++-dev - General purpose cryptographic library - C++ development
+#libcrypto++-doc - General purpose cryptographic library - documentation
+#libcrypto++-utils - General purpose cryptographic library - utilities and data files
+#libcrypto++9 - General purpose cryptographic library - shared library
+#libcrypto++9-dbg - General purpose cryptographic library - debug symbols
+#python-xdg - Python 2 library to access freedesktop.org standards
+#python3-xdg - Python 3 library to access freedesktop.org standards
+#python-gmenu - GNOME implementation of the freedesktop menu specification
+#python-gmenu-dbg - Python bindings for the freedesktop menu specification for GNOME (debug extension)
 
-python-gtk2-dev - GTK+ bindings: devel files
+# в конце хотел добавить, если что-то нужно сделать и не знаем где взять
+# инфу, смотрим в архив с исходным кодом, там всегда почти есть папочка doc
+# и файлы с заглавными именами вроде : README INSTALL AUTHORS LICENSE
+# НЕ боимся их читать
+# там в основном должна быть вся информация как собрать пакет и не только
 
-python-xmpp - Python library for communication with XMPP (Jabber) servers
-python-pyxmpp - XMPP and Jabber implementation for Python
-python-pyxmpp-doc - XMPP and Jabber implementation for Python (documentation)
-python-sleekxmpp - Python XMPP (Jabber) Library Implementing Everything as a Plugin
-python-wokkel - collection of enhancements for Twisted
-python3-sleekxmpp - Python XMPP (Jabber) Library Implementing Everything as a Plugin
-
-intltool - Utility scripts for internationalizing XML
-
-python3-openssl - Python 3 wrapper around the OpenSSL library
-python-openssl - Python 2 wrapper around the OpenSSL library
-python-openssl-dbg - Python 2 wrapper around the OpenSSL library (debug extension)
-python-openssl-doc - Python wrapper around the OpenSSL library (documentation package)
-python3-openssl - Python 3 wrapper around the OpenSSL library
-python3-openssl-dbg - Python 3 wrapper around the OpenSSL library (debug extension)
-
-python-pyasn1 - ASN.1 library for Python (Python 2 module)
-python-pyasn1-modules - Collection of protocols modules written in ASN.1 language
-python3-pyasn1 - ASN.1 library for Python (Python 3 module)
-
-libcrypto++-dev - General purpose cryptographic library - C++ development
-libcrypto++-doc - General purpose cryptographic library - documentation
-libcrypto++-utils - General purpose cryptographic library - utilities and data files
-libcrypto++9 - General purpose cryptographic library - shared library
-libcrypto++9-dbg - General purpose cryptographic library - debug symbols
-python-crypto - cryptographic algorithms and protocols for Python
-python-crypto-dbg - cryptographic algorithms and protocols for Python (debug extension)
-python-crypto-doc - cryptographic algorithms and protocols for Python (documentation)
-python-itsdangerous - Various helpers to pass trusted data to untrusted environment - python 2.x
-python-m2crypto - a crypto and SSL toolkit for Python
-python3-crypto - cryptographic algorithms and protocols for Python 3
-python3-crypto-dbg - cryptographic algorithms and protocols for Python 3 (debug extension)
-
-libxss-dev - X11 Screen Saver extension library (development headers)
-libxss1 - X11 Screen Saver extension library
-libxss1-dbg - X11 Screen Saver extension library (debug package)
-
-dbus-x11 - simple interprocess messaging system (X11 deps)
-dbus - simple interprocess messaging system (daemon and utilities)
-libdbus-1-3 - simple interprocess messaging system (library)
-libdbus-1-dev - simple interprocess messaging system (development headers)
-libdbus-glib-1-2 - simple interprocess messaging system (GLib-based shared library)
-libdbus-glib-1-2-dbg - simple interprocess messaging system (GLib library debug symbols)
-libdbus-glib-1-dev - simple interprocess messaging system (GLib interface)
-libdbus-glib-1-doc - simple interprocess messaging system (GLib library documentation)
-libdbusmenu-glib-dev - library for passing menus over DBus - development files
-libdbusmenu-glib-doc - library for passing menus over DBus - documentation
-libdbusmenu-glib4 - library for passing menus over DBus
-
-python-avahi - Python utility package for Avahi
-
-dnsutils - Clients provided with BIND
-
-aspell-en - English dictionary for GNU Aspell
-libgtkspell-dev - Development files for GtkSpell
-libgtkspell0 - a spell-checking addon for GTK's TextView widget
-libgtkspell3-3-0 - spell-checking addon for GTK+'s TextView widget
-libgtkspell3-3-dev - Development files for GtkSpell
-aspell-ru - Russian dictionary for Aspell
-python-gtkspell - Python bindings for the GtkSpell library
-python-gtkspellcheck - spellchecking library written in Python for Gtk based on Enchant
-python-gtkspellcheck-doc - Python GTK Spellcheck common documentation
-python3-gtkspellcheck - spellchecking library written in Python for Gtk based on Enchant
-
-python-gnome2 - Python bindings for the GNOME desktop environment
-python-gnome2-desktop-dev - Python bindings for the GNOME desktop environment
-python-gnome2-dev - Python bindings for the GNOME desktop environment - development files
-python-gnome2-doc - Python bindings for the GNOME desktop environment
-
-python-gnome2 - Python bindings for the GNOME desktop environment
-python-gnome2-desktop-dev - Python bindings for the GNOME desktop environment
-python-gnome2-dev - Python bindings for the GNOME desktop environment - development files
-python-gnome2-doc - Python bindings for the GNOME desktop environment
-python-xdg - Python 2 library to access freedesktop.org standards
-python3-xdg - Python 3 library to access freedesktop.org standards
-python-gmenu - GNOME implementation of the freedesktop menu specification
-python-gmenu-dbg - Python bindings for the freedesktop menu specification for GNOME (debug extension)
-
-notification-daemon - daemon for displaying passive pop-up notifications
-python-notify - Python bindings for libnotify
-
-?
-python-indicate - Python bindings for libindicate
-python-notify2 - desktop notifications API for Python
-python3-notify2 - desktop notifications API for Python 3
